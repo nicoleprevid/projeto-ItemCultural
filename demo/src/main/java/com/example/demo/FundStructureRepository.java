441458013967548bf0a20553d04e67e953245675
Nicole Previd <120424229+nicoleprevid@users.noreply.github.com>
2024-11-03 19:03:07 -0300

    versao java

--- demo/src/main/java/com/example/demo/FundStructureRepository.java
+++ demo/src/main/java/com/example/demo/FundStructureRepository.java
@@ -1,12 +1,11 @@
 package com.example.demo;
 
-import com.example.demo.FundStructure;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface FundStructureRepository extends JpaRepository<FundStructure, Long> {
